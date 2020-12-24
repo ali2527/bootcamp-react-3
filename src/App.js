2f2d31f706357056b911813db7d44c8d8fdd6168
ali Hussain <ali.talha3357@gmail.com>
2020-12-24 15:55:29 +0500

    testing git actions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo2.png';
 import './App.css';
 
 function App() {
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World
-        </p>
-       
+              
       </header>
     </div>
   );
